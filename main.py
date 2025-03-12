4108336abdae573975b129e7cd89a6e40a6b3753
Alastor-radio-demon <mohamedaziz.khiari19@gmail.com>
2025-03-12 13:35:34 +0100

    commit 16

--- main.py
+++ main.py
@@ -1,15 +1,7 @@
-<<<<<<< HEAD
-def somme (L) :
-  s=0
-  for e in L:
-    s+=e
-  return s
-
 my_list=[1,5,7]
-som=somme (my_list)
-print ('la somme est =', som)
-=======
-my_list=[1,5,7]
-print('la somme est',sum(my_list))
-
->>>>>>> feature_list
+if my list:
+  print ('la somme est: ', sum(my_list) )
+  print ('le max est:', max(my list) )
+  print ('le min est:',min(my_list) )
+else:
+  print ('liste vide')
